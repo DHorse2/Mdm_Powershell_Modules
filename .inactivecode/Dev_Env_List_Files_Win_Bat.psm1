17f922c3d7df58bdb4d4e04580d29d086a173dcf
David G. Horsman <david_horsman@hotmail.com>
2025-04-06 19:16:22 -0700

    perform rm --cache commands to remove local items

--- .inactivecode/Dev_Env_List_Files_Win_Bat.psm1
+++
@@ -1,7 +0,0 @@
-Rem  NOT CURRENT
-Echo off
-Rem Dev_Env_List_Modules_Win_Bat
-Rem 
-Echo on
-Dir %1 /S
-Echo " ."
--- .inactivecode/Mdm_Std_Library/Get-FileNamesFromPath.ps1
+++
@@ -1,9 +0,0 @@
-# Get-FileNamesFromPath
-
-function Get-FileNamesFromPath ($SourcePath) {
-    $SourceFileNames = Get-ChildItem -Path $SourcePath -File | ForEach-Object { $_.BaseName }
-    # ForEach-Object $SourceFileNames {
-        
-    # }
-    $SourceFileNames
-}
